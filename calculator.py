c4d8803d0e6654c55c2c1b96e548cd9133d452d6
Jimba <jimmybee@vk.vom>
2023-05-09 08:50:44 +0300

    Revert "exponentiation"
    This reverts commit 25d29b7039ef9836501db37f2f777bc5c9faebde.

--- calculator.py
+++ calculator.py
@@ -1,22 +1,14 @@
 inp = input().split()
 
-if len(inp) >= 3:
-    if inp[0].isnumeric() and inp[2].isnumeric():
-        first, second = float(inp[0]), float(inp[2])
-        if inp[1] == '+':
-            result = first + second
-        elif inp[1] == '-':
-            result = first - second
-        elif inp[1] == '*':
-            result = first * second
-        elif inp[1] == '/':
-            result = first / second
-        elif inp[1] == '**':
-            result = first ** second
-        else:
-            result = 0
-        print(result)
-    else:
-        print('Введите числа!')
+first, second = float(inp[0]), float(inp[2])
+if inp[1] == '+':
+    result = first + second
+elif inp[1] == '-':
+    result = first - second
+elif inp[1] == '*':
+    result = first * second
+elif inp[1] == '/':
+    result = first / second
 else:
-    print('Некорректный ввод! Введите по шаблону: "2 + 2"')
+    result = 0
+print(result)
