25d29b7039ef9836501db37f2f777bc5c9faebde
Jimba <jimmybee@vk.vom>
2023-05-09 08:48:49 +0300

    exponentiation

--- calculator.py
+++ calculator.py
@@ -1,14 +1,22 @@
 inp = input().split()
 
-first, second = float(inp[0]), float(inp[2])
-if inp[1] == '+':
-    result = first + second
-elif inp[1] == '-':
-    result = first - second
-elif inp[1] == '*':
-    result = first * second
-elif inp[1] == '/':
-    result = first / second
+if len(inp) >= 3:
+    if inp[0].isnumeric() and inp[2].isnumeric():
+        first, second = float(inp[0]), float(inp[2])
+        if inp[1] == '+':
+            result = first + second
+        elif inp[1] == '-':
+            result = first - second
+        elif inp[1] == '*':
+            result = first * second
+        elif inp[1] == '/':
+            result = first / second
+        elif inp[1] == '**':
+            result = first ** second
+        else:
+            result = 0
+        print(result)
+    else:
+        print('Введите числа!')
 else:
-    result = 0
-print(result)
+    print('Некорректный ввод! Введите по шаблону: "2 + 2"')
