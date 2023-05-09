e13457bfb1b594527bb6df1c260f8ed16979214f
Jimba <jimmybee@vk.vom>
2023-05-09 08:16:24 +0300

    division by zero processed

--- calculator.py
+++ calculator.py
@@ -1,14 +1,17 @@
 inp = input().split()
 
 first, second = float(inp[0]), float(inp[2])
-if inp[1] == '+':
-    result = first + second
-elif inp[1] == '-':
-    result = first - second
-elif inp[1] == '*':
-    result = first * second
-elif inp[1] == '/':
-    result = first / second
+if not second == 0:
+    if inp[1] == '+':
+        result = first + second
+    elif inp[1] == '-':
+        result = first - second
+    elif inp[1] == '*':
+        result = first * second
+    elif inp[1] == '/':
+        result = first / second
+    else:
+        result = 0
+    print(result)
 else:
-    result = 0
-print(result)
+    print('Деление на ноль!')
